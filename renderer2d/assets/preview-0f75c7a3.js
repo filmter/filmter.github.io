06ce3a7bfe11d3892b8c9f83e0ef31818b158f9a
ZhongxuYang <himatthew@foxmail.com>
2023-06-08 12:20:02 +0000

    Deploying to main from  @ ada18d3d77c8b6befe83b289eb692b1c40bea1bc 🚀

--- renderer2d/assets/preview-0f75c7a3.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./iframe-c0db9d09.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-EYKKDMVH-43daef81.js").then(e=>e.D),["./DocsRenderer-EYKKDMVH-43daef81.js","./iframe-c0db9d09.js","./_commonjsHelpers-725317a4.js","./index-d475d2ea.js","./index-d37d4223.js","./doctrine-adde9140.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-0f75c7a3.js.map
